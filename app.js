5602270ea6c5066e9dee446c499253396436212b
Rushikesh Dhanawade <rushidhanawade123@gmail.com>
2025-10-04 19:35:56 +0530

    uploaded files from another laptop

---
+++ app.js
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom/client";
+
+const TestComponent = () => {
+  return (
+    <>
+      <h1>Welcome to Rushi's Portfolio</h1>
+    </>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(<TestComponent />);
